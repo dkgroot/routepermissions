9141aee98b0f8c1c1a3c35ac28704aa4907661ea
Rob Thomas <xrobau@gmail.com>
2009-03-31 03:53:35 +0000

    It now updates the database properly

--- functions.inc.php
+++ functions.inc.php
@@ -29,14 +29,14 @@ function routepermissions_configpageinit($pagename) {
         // On a 'new' user, 'tech_hardware' is set, and there's no extension. Hook into the page.
         if ($tech_hardware != null || $pagename == 'users') {
                 rp_applyhooks();
-                $currentcomponent->addprocessfunc('rp_configprocess', 8);
+                $currentcomponent->addprocessfunc('rp_configprocess', 5);
         } elseif ($action=="add") {
                 // We don't need to display anything on an 'add', but we do need to handle returned data.
-                $currentcomponent->addprocessfunc('rp_configprocess', 8);
+                $currentcomponent->addprocessfunc('rp_configprocess', 5);
         } elseif ($extdisplay != '') {
                 // We're now viewing an extension, so we need to display _and_ process.
                 rp_applyhooks();
-                $currentcomponent->addprocessfunc('rp_configprocess', 8);
+                $currentcomponent->addprocessfunc('rp_configprocess', 5);
         }
 }
 
@@ -64,10 +64,35 @@ function rp_configpageload() {
                 $section = _('Outbound Route Permssions');
 		$routes = rp_get_routes();
 		foreach ($routes as $route) {
-			$currentcomponent->addguielem($section, new gui_radio($route, $currentcomponent->getoptlist('rpyn'), rp_get_perm($extdisplay,$route), $route, "" , null));
+			$currentcomponent->addguielem($section, new gui_radio("rp_$route", $currentcomponent->getoptlist('rpyn'), rp_get_perm($extdisplay,$route), $route, "" , null));
 		}
 	}
 }
+
+function rp_configprocess() {
+	// Extract any variables from $REQUEST that start with rp_
+        $action = isset($_REQUEST['action'])?$_REQUEST['action']:null;
+        $extdisplay = isset($_REQUEST['extdisplay'])?$_REQUEST['extdisplay']:null;
+
+	foreach ($_REQUEST as $r=>$val) {
+		if (!strncmp($_REQUEST[$r], "rp_", 3)) {
+			$rps[substr($r, 3)]=$val;
+		}
+	}
+        //if submitting form, update database
+        switch ($action) {
+                case "add":
+                case "edit":
+		rp_purge_ext($extdisplay);
+		rp_set_perm($extdisplay, $rps);
+                break;
+                case "del":
+		rp_purge_ext($extdisplay);
+                break;
+        }
+}
+
+
 
 function rp_get_perm($ext, $route) {
 	global $db;
@@ -82,6 +107,27 @@ function rp_get_perm($ext, $route) {
 		return "YES";
 	}
 }
+
+function rp_set_perm($ext, $rps) {
+	global $db;
+	$Sext = mysql_real_escape_string($ext);
+	foreach($rps as $r=>$p) {
+		$val = explode("=", $p);
+		$Sr =mysql_real_escape_string($r);
+		$Sval =mysql_real_escape_string($val[1]);
+		$sql = "INSERT INTO routepermissions (exten, routename, allowed) VALUES ('$Sext', '$Sr', '$Sval')";
+		sql($sql);
+	}
+}
+
+function rp_purge_ext($ext) {
+	global $db;
+	$Sext = mysql_real_escape_string($ext);
+	$sql = "DELETE FROM routepermissions WHERE exten='$Sext'";
+	sql($sql);
+}
+	
+	
 
 // When outbound routes is rewritten to no longer use the 'extensions' database, the following function will need to be changed
 
