8b8bb87e29cddafb56143a560aa426ab2efb5f99
Michael Newton <miken32@gmail.com>
2017-02-11 21:25:27 -0800

    move all functions into class

--- Routepermissions.class.php
+++ Routepermissions.class.php
@@ -1,4 +1,5 @@
 <?php
+
 // Based on an original Release by Rob Thomas (xrobau@gmail.com)
 // Copyright Rob Thomas (2009)
 // Extensive modifications by Michael Newton (miken32@gmail.com)
@@ -18,11 +19,7 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-if (!class_exists("RouterpermissionsLegacy")) {
-    require_once(dirname(__FILE__) . "/functions.inc.php");
-}
-
-class Routepermissions extends RoutepermissionsLegacy implements FreePBX\BMO
+class Routepermissions extends \FreePBX\FreePBX_Helpers implements \FreePBX\BMO
 {
     /** Constructor code for version 13+
      * @param Object $freepbx The FreePBX object
@@ -127,8 +124,314 @@ class Routepermissions extends RoutepermissionsLegacy implements FreePBX\BMO
         return;
     }
 
-    public function doConfigPageInit($display)
+    /**
+     * Tell the system we want to hook into the dialplan
+     */
+    public function myDialplanHooks()
     {
-        return;
+        // signal our intent to hook into the dialplan
+        return true;
+    }
+
+    /**
+     * The actual dialplan hook
+     */
+    public function doDialplanHook(&$ext, $engine, $pri)
+    {
+        if ($engine !== "asterisk") {
+            return false;
+        }
+
+        foreach ($ext->_exts as $context=>$extensions) {
+            if (strncmp($context, "macro-dialout-", 14) === 0) {
+                $ext->splice($context, "s", 1, new \ext_agi("checkperms.agi"));
+                $ext->add($context, "barred", 1, new \ext_noop("Route administratively banned for this user."));
+                $ext->add($context, "barred", 2, new \ext_hangup());
+                $ext->add($context, "reroute", 1, new \ext_goto("1", "\${ARG2}", "from-internal"));
+            }
+        }
+
+        // Insert the ROUTENAME into each route
+        $names = core_routing_list();
+        foreach ($names as $name) {
+            $context = "outrt-$name[route_id]";
+            $routename = $name["name"];
+            $routes = core_routing_getroutepatternsbyid($name["route_id"]);
+            foreach ($routes as $rt) {
+                $extension = $rt["match_pattern_prefix"] . $rt["match_pattern_pass"];
+                // If there are any wildcards in there, add a _ to the start
+                if (preg_match("/\.|z|x|\[|\]/i", $extension)) {
+                    $extension = "_".$extension;
+                }
+                $ext->splice($context, $extension, 1, new \ext_setvar("__ROUTENAME", $routename));
+            }
+        }
+    }
+
+    /**
+     * Tell the system which modules' pages we want to hook (hooked)
+     *
+     * @return array All the modules we want to hook into
+     */
+    public static function myGuiHooks() {
+        // extensions page is part of core.
+        return ["core"];
+    }
+
+    /**
+     * Perform our hook actions on page display (hooked)
+     *
+     * @param Object $currentcomponent The ugly old page object
+     * @param string $module The module name
+     * @return boolean Returns false if the page is not modified
+     */
+    public function doGuiHook(&$currentcomponent, $module) {
+        $pagename   = isset($_REQUEST["display"]) ? $_REQUEST["display"] : "";
+        $extdisplay = isset($_REQUEST["extdisplay"]) ? $_REQUEST["extdisplay"] : "";
+        $action     = isset($_REQUEST["action"]) ? $_REQUEST["action"] : "";
+        $i          = 0;
+
+        if (
+            $module !== "core" || $action === "del" || empty($extdisplay) ||
+            ($pagename !== "extensions" && $pagename !== "users")
+        ) {
+            return false;
+        }
+
+        $routes = $this->getRoutes();
+        $stmt = $db->prepare("SELECT allowed, faildest, prefix FROM routepermissions WHERE routename = ? AND exten = ?");
+        foreach ($routes as $route) {
+            // in 13 this is PDO, returns boolean; in 11 it's PEAR, returns a result object
+            $res = $this->db->execute($stmt, array($route, $extdisplay));
+            if (DB::isError($res) || $res === false) {
+                continue;
+            } elseif ($res === true) {
+                list($allowed, $faildest, $prefix) = $stmt->fetch(\PDO::FETCH_NUM);
+            } elseif ($res->numRows() === 1) {
+                list($allowed, $faildest, $prefix) = $res->fetchRow();
+            } else {
+                $allowed = "YES";
+                $faildest = "";
+            }
+            if ($allowed === "NO" && !empty($prefix)) {
+                $allowed = "REDIRECT";
+            }
+            $route = htmlspecialchars($route);
+            $yes = _("Allow");
+            $no = _("Deny");
+            $redirect = _("Redirect w/prefix");
+            $i += 10;
+            $js = '$("input[name=" + this.name.replace(/^routepermissions_perm_(\d+)-(.*)$/, "routepermissions_prefix_$1-$2") + "]").val("").prop("disabled", (this.value !== this.name + "=REDIRECT")).prop("required", (this.value === this.name + "=REDIRECT"));var id=$("select[name=" + $("#" + this.name.replace(/^routepermissions_perm_(\d+)-(.*)$/, "routepermissions_faildest_$1-$2")).val() + "]").val("").change().prop("disabled", (this.value !== this.name + "=NO")).data("id");$("select[data-id=" + id + "]").prop("disabled", (this.value !== this.name + "=NO"))';
+            $radio = new \gui_radio(
+                "routepermissions_perm_$i-$route", //element name
+                array(
+                    array("value"=>"YES", "text"=>$yes),
+                    array("value"=>"NO", "text"=>$no),
+                    array("value"=>"REDIRECT", "text"=>$redirect),
+                ), //radios
+                $allowed, //current value
+                sprintf(_("Allow access to %s"), $route), //group label text
+                "", //help text
+                false, //disable
+                htmlspecialchars($js), //onclick (13+)
+                "", //class (13+)
+                true //paired values; needed for 11 compatibility (13+)
+            );
+            $currentcomponent->addguielem($section, $radio);
+
+            $selects = new \gui_drawselects(
+                "routepermissions_faildest_$i-$route", //element name
+                $i, //index
+                $faildest, //current value
+                sprintf(_("Failure destination for %s"), $route), //label text
+                "", //help text
+                false, //can be empty
+                "", //fail validation message
+                _("Use default"), //empty message
+                ($allowed !== "NO"), //disable (13+)
+                "" //class (13+)
+            );
+            $currentcomponent->addguielem($section, $selects);
+
+            $input = new \gui_textbox(
+                "routepermissions_prefix_$i-$route", //element name
+                $prefix, //current value
+                sprintf(_("Redirect prefix for %s"), $route), //label text
+                "", //help text
+                "", //js validation
+                "", //validation failure msg
+                true, //can be empty
+                0, //maxchars
+                ($allowed !== "REDIRECT"), // disable; no way to re-enable in 11/12
+                false, //input group (13+)
+                "", //class (13+)
+                true //autocomplete (13+)
+            );
+            $currentcomponent->addguielem($section, $input);
+        }
+    }
+
+    /**
+     * Tell the system which pages' POSTs we want to hook (hooked)
+     *
+     * @return array All the pages (not modules) we want to hook into
+     */
+    public static function myConfigPageInits() {
+        return ["extensions", "users"];
+    }
+
+    /**
+     * Perform our hook action on page POST (hooked)
+     *
+     * @param string $module The module name
+     * @return boolean Returns false if there's nothing to do
+     */
+    public function doConfigPageInit($module)
+    {
+        $pagename    = isset($_REQUEST["display"]) ? $_REQUEST["display"] : "index";
+        $extdisplay  = isset($_REQUEST["extdisplay"]) ? $_REQUEST["extdisplay"] : null;
+        $action      = isset($_REQUEST["action"]) ? $_REQUEST["action"] : null;
+        $route_perms = array();
+        if (empty($extdisplay) || empty($action)) {
+            return false;
+        }
+        foreach ($_POST as $k=>$v) {
+            if (!preg_match("/routepermissions_(faildest|perm|prefix)_\d+-(.*)/", $k, $matches)) {
+                continue;
+            }
+            $route_name = $matches[2];
+            if (!isset($route_perms[$route_name])) {
+                $route_perms[$route_name] = array("faildest"=>null, "perms"=>"YES", "prefix"=>null);
+            }
+            switch ($matches[1]) {
+                case "faildest":
+                    $faildest_index = substr($v, 4); // remove "goto" from value
+                    $faildest_type = isset($_POST[$v]) ? $_POST[$v] : null;
+                    if ($faildest_type && isset($_POST["$faildest_type$faildest_index"])) {
+                        $route_perms[$route_name]["faildest"] = $_POST["$faildest_type$faildest_index"];
+                    }
+                    break;
+                case "perm":
+                    list($foo, $perm) = explode("=", $v, 2);
+                    $route_perms[$route_name]["perms"] = $perm;
+                    break;
+                case "prefix":
+                    $route_perms[$route_name]["prefix"] = $v;
+                    break;
+            }
+        }
+        if (count($route_perms) === 0) {
+            return false;
+        }
+
+        switch ($action) {
+            case "add":
+            case "edit":
+                $result = $this->db->query("DELETE FROM routepermissions WHERE exten = ?", $extdisplay);
+                $stmt = $this->db->prepare("INSERT INTO routepermissions (exten, routename, allowed, faildest, prefix) VALUES(?, ?, ?, ?, ?)");
+                foreach($route_perms as $route_name=>$data) {
+                    if ($data["perms"] === "REDIRECT") {
+                        $data["faildest"] = null;
+                        $data["perms"] = "NO";
+                        if (empty($data["prefix"])) {
+                            $data["prefix"] = null;
+                        }
+                    } elseif ($data["perms"] === "NO") {
+                        $data["prefix"] = null;
+                    } else {
+                        $data["perms"] = "YES";
+                        $data["faildest"] = $data["prefix"] = null;
+                    }
+                    $res = $this->db->execute($stmt, array(
+                        $extdisplay,
+                        $route_name, 
+                        $data["perms"],
+                        $data["faildest"],
+                        $data["prefix"],
+                    ));
+                }
+                break;
+            case "del":
+                $this->db->query("DELETE FROM routepermissions WHERE exten = ?", $extdisplay);
+                break;
+        }
+    }
+
+    public function getRoutes()
+    {
+        $sql = "SELECT DISTINCT name FROM outbound_routes JOIN outbound_route_sequence USING (route_id) ORDER BY seq";
+        $result = $this->db->getCol($sql);
+        return $result;
+    }
+
+    public function getDefaultDest()
+    {
+        $sql = "SELECT faildest FROM routepermissions where exten = -1 LIMIT 1";
+        $result = $this->db->getOne($sql);
+        return $result;
+    }
+
+    public function updateDefaultDest($dest)
+    {
+        $sql = "DELETE FROM routepermissions WHERE exten = -1";
+        $this->db->query($sql);
+        if (DB::isError($res)) {
+            return false;
+        }
+        if (!empty($dest)) {
+            $sql = "INSERT INTO routepermissions (exten, routename, faildest, prefix) VALUES ('-1', 'default', ?, '')";
+            $res = $this->db->query($sql, array($dest));
+            if (DB::isError($res)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public function setRangePermissions($route, $range, $allowed, $faildest = "", $prefix = "") {
+        $allowed = (strtoupper($allowed) === "NO") ? "NO" : "YES";
+        $extens = array_intersect(
+            $sys_ext = array_map(function($u) {return $u[0];}, core_users_list()),
+            $ext_range = (strtoupper($range) === strtoupper(_("All"))) ? $sys_ext : self::getRange($range)
+        );
+        if (count($extens) === 0) {
+            return false;
+        }
+        $sql = "DELETE FROM routepermissions WHERE exten=? AND routename=?";
+        $stmt1 = $this->db->prepare($sql);
+        $sql = "INSERT INTO routepermissions (exten, routename, allowed, faildest, prefix) VALUES (?, ?, ?, ?, ?)";
+        $stmt2 = $this->db->prepare($sql);
+        foreach ($extens as $ext) {
+            $res = $this->db->execute($stmt1, array($ext, $route));
+            if (DB::isError($res) || $res === false) {
+                return $res;
+            }
+            $res = $this->db->execute($stmt2, array($ext, $route, $allowed, $faildest, $prefix));
+            if (DB::isError($res) || $res === false) {
+                return $res;
+            }
+        }
+        return true;
+    }
+
+    private static function getRange($range_str) {
+        $range_out = array();
+        // Strip spaces
+        $ranges = explode(",", str_replace(" ", "", $range_str));
+
+        foreach($ranges as $range) {
+            if (is_numeric($range)) {
+                // Just a number; add it to the list.
+                $range_out[] = $range;
+            } elseif (strpos($range, "-")) {
+                list($start, $end) = explode("-", $range);
+                if (is_numeric($start) && is_numeric($end) && $start < $end) {
+                    for ($i = $start; $i <= $end; $i++) {
+                        $range_out[] = $i;
+                    }
+                }
+            }
+        }
+        return array_unique($range_out, SORT_NUMERIC);
     }
 }
